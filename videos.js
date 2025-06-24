f6b7b755754e2f446fec4ef52ec9bdd1daaf55ff
DarthTaksil <eisler482@gmail.com>
2025-06-24 18:34:31 -0400

    Video List Init Update

--- videos.js
+++ videos.js
@@ -1,5 +1,17 @@
 const videoList = [
-  "VaLYCJq-HvM",
-  "Dfzqr6b9nxM",
-  "zlH4ZvEDopA"
+"VaLYCJq-HvM",
+"Dfzqr6b9nxM",
+"zlH4ZvEDopA",
+"jMeK-BiRpYI",
+"3OrI8DiD2EA",
+"pnKKUmlemMo",
+"CXlK9KpUUkE",
+"Uz62YM-W_mw",
+"fU5aqWlJcXM",
+"H7ON7bMyKbQ",
+"Vy71T-cotX8",
+"4M7ruGE0OxA",
+"9TCm_wetRpE",
+"p_X0ABpNfog",
+"JEb6lPyJf0s"
 ];
